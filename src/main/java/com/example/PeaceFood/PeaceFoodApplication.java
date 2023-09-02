bd1223897b165d20e57945c432b087226fa1e1ae
Dartaniantes <demkacorporation@gmail.com>
2023-09-02 20:15:09 +0300

    project created

---
+++ src/main/java/com/example/PeaceFood/PeaceFoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.PeaceFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeaceFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeaceFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PeaceFood/PeaceFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PeaceFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeaceFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
